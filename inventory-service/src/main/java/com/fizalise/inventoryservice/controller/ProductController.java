c8b5c23993db628348a94545c037b5ff3bb5e99d
Максим Фоминцев <max135603@gmail.com>
2025-02-10 02:57:08 +0300

    Нейминг

--- inventory-service/src/main/java/com/fizalise/inventoryservice/controller/ProductController.java
+++ inventory-service/src/main/java/com/fizalise/inventoryservice/controller/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/inventory/product-list")
 public record ProductController(ProductService productService) {
     @GetMapping
     public List<ProductItem> getAllItems(
