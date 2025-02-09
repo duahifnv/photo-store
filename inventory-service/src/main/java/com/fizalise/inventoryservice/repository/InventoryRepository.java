6fdea287184aecc0c34e1cdb040d11aba9bc4d81
Максим Фоминцев <max135603@gmail.com>
2025-02-10 02:58:43 +0300

    Метод

--- inventory-service/src/main/java/com/fizalise/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/fizalise/inventoryservice/repository/InventoryRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InventoryRepository extends JpaRepository<InventoryItem, String> {
+    boolean existsBySkuCodeAndQuantityGreaterThanEqual(String skuCode, Integer quantity);
 }
