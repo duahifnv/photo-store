38fe1da97542456562c03ce559a4de9eadeeb58f
Максим Фоминцев <max135603@gmail.com>
2025-02-10 02:55:49 +0300

    JPA Entity

---
+++ order-service/src/main/java/com/fizalise/orderservice/entity/Order.java
@@ -0,0 +1,33 @@
+package com.fizalise.orderservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Table(name = "orders", schema = "public", catalog = "order_service")
+@Data
+public class Order {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "order_id")
+    private UUID orderId;
+    @Basic
+    @Column(name = "sku_code")
+    private String skuCode;
+    @Basic
+    @Column(name = "price")
+    private BigDecimal price;
+    @Basic
+    @Column(name = "quantity")
+    private Integer quantity;
+    @Basic
+    @Column(name = "customer_email")
+    private String customerEmail;
+    @Basic
+    @Column(name = "order_timestamp")
+    private Timestamp orderTimestamp;
+}
