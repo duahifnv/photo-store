d04f43d19a050ab886396ae2f4d727e69fbb164e
Максим Фоминцев <max135603@gmail.com>
2025-02-09 16:50:08 +0300

    Категория продукта

---
+++ inventory-service/src/main/java/com/fizalise/inventoryservice/entity/ProductCategory.java
@@ -0,0 +1,22 @@
+package com.fizalise.inventoryservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "product_categories", schema = "public", catalog = "inventory_service")
+@Data
+public class ProductCategory {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "category_code")
+    private String categoryCode;
+    @Basic
+    @Column(name = "name")
+    private String name;
+    @Basic
+    @Column(name = "description")
+    private String description;
+}
