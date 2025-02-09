bbbc0953271fb98669b13cbfb86b8811f918589e
Максим Фоминцев <max135603@gmail.com>
2025-02-10 02:57:44 +0300

    Проверка наличия товара на складе и обновление склада

--- inventory-service/src/main/java/com/fizalise/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/fizalise/inventoryservice/controller/InventoryController.java
@@ -1,12 +1,10 @@
 package com.fizalise.inventoryservice.controller;
 
+import com.fizalise.inventoryservice.dto.InventoryUpdate;
 import com.fizalise.inventoryservice.entity.InventoryItem;
 import com.fizalise.inventoryservice.service.InventoryService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,5 +15,15 @@ public record InventoryController(InventoryService inventoryService) {
     @ResponseStatus(HttpStatus.OK)
     public List<InventoryItem> getAllItems() {
         return inventoryService.findAllItems();
+    }
+    @GetMapping("/in-stock")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
+        return inventoryService.isInStock(skuCode, quantity);
+    }
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void updateInventory(@RequestBody InventoryUpdate inventoryUpdate) {
+        inventoryService.updateInventory(inventoryUpdate);
     }
 }
