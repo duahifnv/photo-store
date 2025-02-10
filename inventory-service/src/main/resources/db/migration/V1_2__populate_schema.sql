0719984a3d2b07639c087dee0639d95a04d977ce
Максим Фоминцев <max135603@gmail.com>
2025-02-10 19:53:25 +0300

    Ренейм SKU-кодов из-за rest специфики

--- inventory-service/src/main/resources/db/migration/V1_2__populate_schema.sql
+++ inventory-service/src/main/resources/db/migration/V1_2__populate_schema.sql
@@ -2,11 +2,11 @@ insert into product_categories (category_code, name)
 values ('CAM', 'Фотоаппараты'), ('LEN', 'Объективы'), ('FLS', 'Вспышки'), ('OPT', 'Оптические приборы');
 
 insert into products (sku_code, name, category_code, price)
-values ('CAM/FUJIF-BL', 'Фотоаппарат Fujifilm X-T5 body черный', 'CAM', 189990.00),
-       ('LEN/CANON-BL', 'Объектив Canon RF 16mm f/2.8 STM', 'LEN', 29900.00),
-       ('FLS/FUJIF-BL', 'Вспышка Godox MF12 для макросъемки', 'FLS', 11560.00);
+values ('CAM-FUJIF-BL', 'Фотоаппарат Fujifilm X-T5 body черный', 'CAM', 189990.00),
+       ('LEN-CANON-BL', 'Объектив Canon RF 16mm f/2.8 STM', 'LEN', 29900.00),
+       ('FLS-FUJIF-BL', 'Вспышка Godox MF12 для макросъемки', 'FLS', 11560.00);
 
 insert into inventory (sku_code, quantity)
-values ('CAM/FUJIF-BL', 12),
-       ('LEN/CANON-BL', 5),
-       ('FLS/FUJIF-BL', 3);
+values ('CAM-FUJIF-BL', 12),
+       ('LEN-CANON-BL', 5),
+       ('FLS-FUJIF-BL', 3);
