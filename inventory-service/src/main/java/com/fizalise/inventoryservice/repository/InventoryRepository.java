8c7d95a60b90030e0efbbb5b175901d3c5a127f5
Максим Фоминцев <max135603@gmail.com>
2025-02-10 19:52:41 +0300

    Методы единичных сущностей

--- inventory-service/src/main/java/com/fizalise/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/fizalise/inventoryservice/repository/InventoryRepository.java
@@ -4,9 +4,12 @@ import com.fizalise.inventoryservice.entity.InventoryItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InventoryRepository extends JpaRepository<InventoryItem, String> {
     boolean existsBySkuCode(String skuCode);
     boolean existsBySkuCodeAndQuantityGreaterThanEqual(String skuCode, Integer quantity);
+    Optional<InventoryItem> findBySkuCode(String skuCode);
     void deleteBySkuCode(String skuCode);
 }
