52d821427d9639d583ae50009ee5a8ad3f4b10da
Максим Фоминцев <max135603@gmail.com>
2025-02-12 23:32:39 +0300

    API-шлюз для всех микросервисов

---
+++ api-gateway/src/test/java/com/fizalise/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fizalise.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
