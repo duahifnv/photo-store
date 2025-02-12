f26154e30ecea45da76594f47dba837055464947
Максим Фоминцев <max135603@gmail.com>
2025-02-13 00:36:53 +0300

    База данных и сущность для пользователей

---
+++ api-gateway/src/main/java/com/fizalise/apigateway/entity/User.java
@@ -0,0 +1,40 @@
+package com.fizalise.apigateway.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(schema = "public", name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+}
