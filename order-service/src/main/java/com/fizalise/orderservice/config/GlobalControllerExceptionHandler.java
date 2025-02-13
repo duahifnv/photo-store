2c1697a2af3ce8df4c6ffe3c2b33926b939887d0
Максим Фоминцев <max135603@gmail.com>
2025-02-14 02:11:53 +0300

    Глобальная обработка исключений

--- order-service/src/main/java/com/fizalise/orderservice/config/GlobalControllerExceptionHandler.java
+++ order-service/src/main/java/com/fizalise/orderservice/config/GlobalControllerExceptionHandler.java
@@ -3,13 +3,22 @@ package com.fizalise.orderservice.config;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fizalise.orderservice.dto.ClientErrorResponse;
+import com.fizalise.orderservice.dto.violation.ValidationErrorResponse;
+import com.fizalise.orderservice.dto.violation.Violation;
 import com.fizalise.orderservice.exception.ServerException;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestControllerAdvice
 @Slf4j
@@ -29,7 +38,42 @@ public class GlobalControllerExceptionHandler {
             throw new ServerException();
         }
     }
+    // Обработка исключений, которые так и не были обработаны
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public void handle() {}
+    public String handle(Exception e) {
+        log.error("Unhandled exception [%s]: %s".formatted(e.getClass().getSimpleName(), e.getMessage()));
+        return "Сбой на сервере";
+    }
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<?> handle(ResponseStatusException e) {
+        log.info("Response status exception: " + e.getMessage());
+        return ResponseEntity.status(e.getStatusCode()).body(e.getReason());
+    }
+    // Обработка ошибок валидации параметров запроса и переменных пути запроса
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse handle(ConstraintViolationException e) {
+        List<Violation> violations = e.getConstraintViolations().stream()
+                .map(v -> new Violation(
+                        v.getPropertyPath().toString(), v.getMessage()
+                )).toList();
+        return new ValidationErrorResponse(violations);
+    }
+    // Обработка ошибок полей тела запроса
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse handle(MethodArgumentNotValidException e) {
+        List<Violation> fieldViolations = e.getBindingResult().getFieldErrors().stream()
+                .map(error -> new Violation(
+                        error.getField(), error.getDefaultMessage()
+                ))
+                .toList();
+        return new ValidationErrorResponse(fieldViolations);
+    }
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void handle(HttpMessageNotReadableException e) {
+        log.warn("HttpMessageNotReadableException: " + e.getHttpInputMessage());
+    }
 }
