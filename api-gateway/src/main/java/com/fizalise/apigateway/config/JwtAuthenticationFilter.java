adfe57908033fa193ed8af54fd6b70609af2d75c
Максим Фоминцев <max135603@gmail.com>
2025-02-13 19:03:30 +0300

    Фильтр для Jwt аутентификации

---
+++ api-gateway/src/main/java/com/fizalise/apigateway/config/JwtAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package com.fizalise.apigateway.config;
+
+import com.fizalise.apigateway.service.JwtService;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private static final String AUTH_HEADER_NAME = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+    private final JwtService jwtService;
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        String authHeader = request.getHeader(AUTH_HEADER_NAME);
+        if (authHeader == null || !authHeader.startsWith(BEARER_PREFIX)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String jwt = authHeader.substring(BEARER_PREFIX.length());
+        String username = jwtService.extractUsername(jwt);
+        // Если пользователь из токена существует и в контексте его нет
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // Собираем токен, который кладется в контекст
+            UsernamePasswordAuthenticationToken userPassToken =
+                    new UsernamePasswordAuthenticationToken(
+                            username,
+                            null,
+                            jwtService.extractRoles(jwt)
+                                    .stream()
+                                    .map(SimpleGrantedAuthority::new)
+                                    .toList()
+                    );
+            SecurityContextHolder.getContext().setAuthentication(userPassToken);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
