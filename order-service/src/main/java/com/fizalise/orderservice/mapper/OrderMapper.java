489491a5db9e5c7742fe90b153e929db7b323376
Максим Фоминцев <max135603@gmail.com>
2025-02-16 02:21:28 +0300

    Avro schema-registry

--- order-service/src/main/java/com/fizalise/orderservice/mapper/OrderMapper.java
+++ order-service/src/main/java/com/fizalise/orderservice/mapper/OrderMapper.java
@@ -1,10 +1,11 @@
 package com.fizalise.orderservice.mapper;
 
-import com.fizalise.orderservice.dto.CreatedOrder;
 import com.fizalise.orderservice.dto.OrderRequest;
 import com.fizalise.orderservice.entity.Order;
+import com.fizalise.orderservice.event.OrderPlacedEvent;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import java.sql.Timestamp;
 import java.time.Instant;
@@ -14,6 +15,15 @@ import java.util.UUID;
 public interface OrderMapper {
     @Mapping(target = "orderId", expression = "java(UUID.randomUUID())")
     @Mapping(target = "orderTimestamp", expression = "java(Timestamp.from(Instant.now()))")
-    Order toOrder(OrderRequest orderRequest, String customerEmail);
-    CreatedOrder toCreatedOrder(Order order);
+    @Mapping(source = "userDetails.email", target = "customerEmail")
+    @Mapping(source = "userDetails.firstName", target = "firstName")
+    @Mapping(source = "userDetails.lastName", target = "lastName")
+    Order toOrder(OrderRequest orderRequest);
+    @Mapping(source = "customerEmail", target = "email")
+    @Mapping(source = "orderId", target = "orderId", qualifiedByName = "idToSequence")
+    OrderPlacedEvent toEvent(Order order);
+    @Named("idToSequence")
+    default CharSequence map(UUID value) {
+        return value.toString();
+    }
 }
