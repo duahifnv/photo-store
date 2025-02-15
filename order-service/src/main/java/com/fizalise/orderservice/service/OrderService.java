a1a97a747024b30cebdf6e637585692e1ab2f467
Максим Фоминцев <max135603@gmail.com>
2025-02-16 02:21:55 +0300

    Отправление сообщения в брокер kafka

--- order-service/src/main/java/com/fizalise/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/fizalise/orderservice/service/OrderService.java
@@ -1,16 +1,18 @@
 package com.fizalise.orderservice.service;
 
 import com.fizalise.orderservice.client.InventoryClient;
-import com.fizalise.orderservice.dto.CreatedOrder;
 import com.fizalise.orderservice.dto.InventoryUpdate;
 import com.fizalise.orderservice.dto.OrderRequest;
 import com.fizalise.orderservice.entity.Order;
+import com.fizalise.orderservice.event.OrderPlacedEvent;
 import com.fizalise.orderservice.exception.ResourceNotFoundException;
 import com.fizalise.orderservice.mapper.OrderMapper;
 import com.fizalise.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Sort;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +26,9 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
     private final OrderMapper orderMapper;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+    @Value("${topics.order-notification}")
+    private String orderNotificationTopic;
     public List<Order> findAllOrders() {
         return orderRepository.findAll(
                 Sort.by(Sort.Direction.DESC, "orderTimestamp")
@@ -34,18 +39,24 @@ public class OrderService {
                 .orElseThrow(ResourceNotFoundException::new);
     }
     @Transactional
-    public CreatedOrder createOrder(OrderRequest orderRequest) {
+    public Order createOrder(OrderRequest orderRequest) {
         inventoryClient.updateInventory(
                 InventoryUpdate.builder()
                         .updateType(InventoryUpdate.UpdateType.EXPENSE)
                         .skuCode(orderRequest.skuCode())
                         .quantity(orderRequest.quantity()).build()
         );
-        Order order = orderMapper.toOrder(orderRequest, orderRequest.userDetails().email());
-        log.info("Создан новый заказ: {}", order.getOrderId());
+        Order order = orderMapper.toOrder(orderRequest);
         orderRepository.save(order);
-        log.info("Заказ {} сохранен в базу данных", order.getOrderId());
-        return orderMapper.toCreatedOrder(order);
+        log.info("Заказ {} сохранен в базу данных", order);
+        sendOrderEvent(orderNotificationTopic, order);
+        return order;
+    }
+    public void sendOrderEvent(String topicId, Order order) {
+        OrderPlacedEvent orderPlacedEvent = orderMapper.toEvent(order);
+        log.info("Отправка OrderPlacedEvent в топик %s...".formatted(topicId));
+        kafkaTemplate.send(topicId, orderPlacedEvent);
+        log.info("Ивент успешно отправлен в топик %s...".formatted(topicId));
     }
     @Transactional
     public void deleteOrderById(UUID id) {
