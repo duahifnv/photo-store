c730c6e133cfe87fe8ed44f4a15556bb08234d7c
Максим Фоминцев <max135603@gmail.com>
2025-02-16 14:41:03 +0300

    Отправка сообщений из топика по почте

--- notification-service/src/main/java/com/fizalise/notificationservice/service/NotificationService.java
+++ notification-service/src/main/java/com/fizalise/notificationservice/service/NotificationService.java
@@ -1,13 +1,45 @@
 package com.fizalise.notificationservice.service;
 
 import com.fizalise.orderservice.event.OrderPlacedEvent;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class NotificationService {
+    private final JavaMailSender mailSender;
+    private static final String serviceEmail = "photostore@mail.ru";
     @KafkaListener(topics = "order-notification")
     public void handleTopic(OrderPlacedEvent orderPlacedEvent) {
-        System.out.printf("Получен объект: %s%n", orderPlacedEvent);
+        log.info("Получен объект: {}", orderPlacedEvent);
+        MimeMessagePreparator preparedEmail = prepareEmail(orderPlacedEvent);
+        try {
+            mailSender.send(preparedEmail);
+            log.info("Письмо успешно отправлено на почту {}", orderPlacedEvent.getEmail());
+        } catch (MailException e) {
+            log.error("Не удалось отправить письмо на почту {}. Причина: {}",
+                    orderPlacedEvent.getEmail(), e.getMessage());
+        }
+    }
+    private MimeMessagePreparator prepareEmail(OrderPlacedEvent orderPlacedEvent) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom(serviceEmail);
+            messageHelper.setTo(orderPlacedEvent.getEmail().toString());
+            messageHelper.setSubject("Ваш заказ успешно оформлен");
+            messageHelper.setText("""
+                    Код вашего заказа: %s
+                                        
+                    Компания PhotoStore благодарит вас за покупку!
+                    """.formatted(orderPlacedEvent.getOrderId().toString()));
+        };
     }
 }
