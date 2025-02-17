020559647a427366ca0e76296d5c037e46453737
Максим Фоминцев <max135603@gmail.com>
2025-02-17 19:10:22 +0300

    Внедрен spring security с обработкой полномочий от шлюза

--- order-service/src/main/java/com/fizalise/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/fizalise/orderservice/controller/OrderController.java
@@ -6,8 +6,10 @@ import com.fizalise.orderservice.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,11 +18,18 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Order> getAllOrders() {
         return orderService.findAllOrders();
     }
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/user")
+    public List<Order> getAllOrders(Principal principal) {
+        return orderService.findAllOrders(principal.getName());
+    }
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Order getOrderById(@PathVariable UUID id) {
@@ -28,9 +37,10 @@ public class OrderController {
     }
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Order createOrder(@Valid @RequestBody OrderRequest orderRequest) {
-        return orderService.createOrder(orderRequest);
+    public Order createOrder(@Valid @RequestBody OrderRequest orderRequest, Principal principal) {
+        return orderService.createOrder(orderRequest, principal.getName());
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteOrderById(@PathVariable UUID id) {
