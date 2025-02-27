f5efb8a08af52bbdbab2c36c10d8f72f768ba572
Максим Фоминцев <max135603@gmail.com>
2025-02-27 23:20:19 +0300

    Swagger UI для эндпоинтов

---
+++ inventory-service/src/main/java/com/fizalise/inventoryservice/config/OpenAPIConfig.java
@@ -0,0 +1,16 @@
+package com.fizalise.inventoryservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI authServiceAPI() {
+        return new OpenAPI()
+                .info(new Info().title("API для сервиса учета товаров")
+                        .version("1.0.0"));
+    }
+}
