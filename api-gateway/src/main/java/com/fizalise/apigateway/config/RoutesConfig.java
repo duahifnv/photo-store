636b4ca62f14bc33644bbd49afcc64d15a80d5dc
Максим Фоминцев <max135603@gmail.com>
2025-02-27 23:20:30 +0300

    Маршрутизация для Swagger

--- api-gateway/src/main/java/com/fizalise/apigateway/config/RoutesConfig.java
+++ api-gateway/src/main/java/com/fizalise/apigateway/config/RoutesConfig.java
@@ -1,18 +1,20 @@
 package com.fizalise.apigateway.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import java.net.URI;
 import java.util.function.Function;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
-
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,8 +23,6 @@ public class RoutesConfig {
     @Bean
     public RouterFunction<ServerResponse> inventoryServiceRoute() {
         return route("inventory-service-route")
-                /* Любой запрос, путь которого имеет паттерн /api/v1/inventory/** будет перенаправлен
-                   на порт 8080 и хост localhost */
                 .route(path("/api/v1/inventory/**"), http("http://localhost:8080"))
                 .before(authenticationFilter)
                 .build();
@@ -40,5 +40,25 @@ public class RoutesConfig {
                 .route(path("/api/v1/auth/**"), http("http://localhost:8083"))
                 .build();
     }
-
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRouteSwagger() {
+        return route("inventory-service-route-swagger")
+                .route(path("/aggregate/inventory-service/v1/api-docs"), http("http://localhost:8080"))
+                .filter(setPath("/api/v1/api-docs"))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRouteSwagger() {
+        return route("order-service-route-swagger")
+                .route(path("/aggregate/order-service/v1/api-docs"), http("http://localhost:8081"))
+                .filter(setPath("/api/v1/api-docs"))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> authenticationServiceRouteSwagger() {
+        return route("authentication-service-route-swagger")
+                .route(path("/aggregate/auth-service/v1/api-docs"), http("http://localhost:8083"))
+                .filter(setPath("/api/v1/api-docs"))
+                .build();
+    }
 }
